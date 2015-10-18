a7e876d9fb315e41d7755dae8ae97e74a875af04
beatrizbu <bzbustamante@gmail.com>
2015-10-18 11:53:56 -0400

    Create c.person.rb

---
+++ c.person.rb
@@ -0,0 +1,53 @@
+module ClassPersonFactory
+class Person
+    attr_accessor :name, :type
+
+    def initialize(name, type)
+      @name = name
+      @type = type
+    end
+
+end
+  person = Person.new('Joe Biden', 'Politician')
+  puts person.name
+  puts person.type
+
+  #   def vote
+  #     if Person.type != Politician
+  #       puts "Choose affiliation:"
+  #       #candidates
+  #     else
+  #       puts Person "#{name}"
+  #     end
+  #   end
+
+
+   class Voter < Person
+    attr_accessor :affiliation
+    def initialize(name, affiliation)
+      super
+      @affiliation = affiliation
+    end
+
+  end
+
+  voter = Voter.new('Bernie Sanders', 'Democrat')
+
+  puts voter.name
+  puts voter.affiliation
+
+
+  class Politician < Voter
+   attr_accessor:party
+   def initialize(name, party)
+     super
+     @party = party
+   end
+
+  end
+  politician = Politician.new('Hilary Clinton', 'Democratic Party')
+
+  puts politician.name
+  puts politician.affiliation
+
+end
